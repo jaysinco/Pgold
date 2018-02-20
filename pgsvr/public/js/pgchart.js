bf68e471d906ea9db32a69d771c7e334657da5c5
jaysinco <jaysinco@163.com>
2018-02-20 19:53:25 +0800

    paper gold view server

---
+++ pgsvr/public/js/pgchart.js
@@ -0,0 +1,225 @@
+$(function () {
+    Highcharts.setOptions({
+        lang: {
+            rangeSelectorZoom: ''
+        },
+        global: {
+            useUTC: false 
+        },
+        chart: {
+            style: {
+              fontFamily: 'Georgia'
+            }
+          }
+    });
+    drawPaperGoldTick();
+    state = "tick"
+});
+
+var state = null;
+
+function toggle() {
+    if (state == "tick") {
+        drawPaperGoldKLine();
+        state = "history";
+    } else if (state == "history") {
+        drawPaperGoldTick();
+        state = "tick";
+    }
+};
+
+function drawPaperGoldTick() {
+    var nday = new Date()
+    var today = nday.getFullYear()+"-"+(nday.getMonth()+1)+"-"+nday.getDate()
+    var start =  Math.floor(Date.parse(new Date(today))/1000);
+    var end = Math.floor(Date.parse(new Date())/1000);
+    $.getJSON('/papergold/price/tick/json/by/timestamp?start='+start+'&end='+end, function (data) {
+        var pgcs = [];
+        for (var i = 0; i < data.length; i += 1) {
+            pgcs.push([
+                data[i].t*1000,
+                data[i].p
+            ])
+        }
+        $('#pg_price').highcharts({
+            chart: {
+                zoomType: 'x',
+                height: 400
+            },
+            credits:{
+                enabled: false
+            },
+            title: {
+                text: 'ICBC Paper Gold Bankbuy Price',
+                style:{
+                    fontWeight:"bold",
+                    fontSize: "16px"
+                }
+            },
+            xAxis: {
+                type: 'datetime',
+                dateTimeLabelFormats: {
+                    millisecond: '%H:%M:%S.%L',
+                    second: '%H:%M:%S',
+                    minute: '%H:%M',
+                    hour: '%H:%M',
+                    day: '%m-%d',
+                    week: '%m-%d',
+                    month: '%Y-%m',
+                    year: '%Y'
+                }
+            },
+            tooltip: {
+                dateTimeLabelFormats: {
+                    millisecond: '%H:%M:%S.%L',
+                    second: '%H:%M:%S',
+                    minute: '%H:%M',
+                    hour: '%H:%M',
+                    day: '%Y-%m-%d',
+                    week: '%m-%d',
+                    month: '%Y-%m',
+                    year: '%Y'
+                }
+            },
+            yAxis: {
+                lineWidth: 2,
+                opposite: true,
+                labels: {
+                    align: 'right',
+                    x: -3
+                },
+                title: {
+                    text: 'CNY'
+                },
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+            series: [{
+                type: 'area',
+                name: 'Paper Gold',
+                data: pgcs
+            }]
+        });
+    });
+};
+
+function drawPaperGoldKLine() {
+    $.getJSON('/papergold/price/kline/json/all/day', function (data) {
+        var pgklines = [];
+        for (var i = 0; i < data.length; i += 1) {
+            pgklines.push([
+                data[i].t*1000,
+                data[i].o,
+                data[i].h,
+                data[i].l,
+                data[i].c
+            ]);
+        }
+        $('#pg_price').highcharts('StockChart', {
+            credits:{
+                enabled: false
+            },
+            chart: {
+                height: 400
+            },
+            rangeSelector: {
+                allButtonsEnabled: true,
+                buttons: [{
+                    type: 'month',
+                    count: 1,
+                    text: 'Day',
+                    dataGrouping: {
+                        forced: true,
+                        units: [['day', [1]]]
+                    }
+                }, {
+                    type: 'all',
+                    text: 'Week',
+                    dataGrouping: {
+                        forced: true,
+                        units: [['week', [1]]]
+                    }
+                }, {
+                    type: 'all',
+                    text: 'Month',
+                    dataGrouping: {
+                        forced: true,
+                        units: [['month', [1]]]
+                    }
+                }],
+                buttonTheme: {
+                    width: 60
+                },
+                selected: 0
+            },
+    
+            title: {
+                text: 'ICBC Paper Gold Bankbuy History',
+                style:{
+                    fontWeight:"bold",
+                }
+            },
+            xAxis: {
+                dateTimeLabelFormats: {
+                    millisecond: '%H:%M:%S.%L',
+                    second: '%H:%M:%S',
+                    minute: '%H:%M',
+                    hour: '%H:%M',
+                    day: '%m-%d',
+                    week: '%m-%d',
+                    month: '%y-%m',
+                    year: '%Y'
+                }
+            },
+            yAxis: [{
+                labels: {
+                    align: 'right',
+                    x: -3
+                },
+                title: {
+                    text: 'CNY'
+                },
+                lineWidth: 2
+            }],
+            series: [{
+                type: 'candlestick',
+                name: 'Paper Gold',
+                color: 'green',
+                lineColor: 'green',
+                upColor: 'red',
+                upLineColor: 'red',
+                navigatorOptions: {
+                    color: 'Silver'
+                },
+                data: pgklines
+            }]
+        });
+    });
+};
---
+++ pgsvr/server.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/binary"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	host := flag.String("h", "127.0.0.1", "server host of postgreSQL")
+	pwd := flag.String("p", "unknown", "login password of postgreSQL")
+	flag.Parse()
+	token := fmt.Sprintf("host=%s password=%s user=root dbname=root sslmode=disable", *host, *pwd)
+	db, err := sql.Open("postgres", token)
+	if err != nil {
+		fmt.Printf("open postgres[%s]: %v\n", token, err)
+		return
+	}
+	if err := db.Ping(); err != nil {
+		fmt.Printf("connect to postgres[%s]: %v\n", token, err)
+		return
+	}
+	defer db.Close()
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/favicon.ico" {
+			http.Redirect(w, r, "/public/favicon.ico", http.StatusMovedPermanently)
+			return
+		}
+		http.Redirect(w, r, "/public/html/home.html", http.StatusMovedPermanently)
+	}))
+	mux.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
+	mux.Handle("/papergold/price/tick/json/by/timestamp", &tickPrice{DB: db, Mode: typeJSON})
+	mux.Handle("/papergold/price/kline/json/all/day", &klinePrice{DB: db})
+
+	server := &http.Server{
+		Addr:    ":80",
+		Handler: mux,
+	}
+	fmt.Printf("jserver is listening on port%s...\n", server.Addr)
+	fmt.Printf("jserver stop: %v\n", server.ListenAndServe())
+}
+
+type klinePrice struct {
+	DB *sql.DB
+}
+
+func (kp *klinePrice) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	pgks, err := queryKLineData(kp.DB)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("query papergold kline data: %v", err), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	encoder := json.NewEncoder(w)
+	encoder.Encode(pgks)
+}
+
+type pgkday struct {
+	Timestamp float64 `json:"t"`
+	Open      float32 `json:"o"`
+	High      float32 `json:"h"`
+	Low       float32 `json:"l"`
+	Close     float32 `json:"c"`
+}
+
+func (p pgkday) String() string {
+	tm := time.Unix(int64(p.Timestamp), 0)
+	return fmt.Sprintf("%4d-%02d-%02d | open: %.2f/ high: %.2f/ low: %.2f/ close: %.2f",
+		tm.Year(), tm.Month(), tm.Day(), p.Open, p.High, p.Low, p.Close)
+}
+
+func queryKLineData(db *sql.DB) ([]pgkday, error) {
+	rows, err := db.Query(`
+		SELECT extract(epoch from to_timestamp(date, 'YYYY-MM-DD')) daytmsp,
+			(SELECT bankbuy FROM pgmkt WHERE txtime = min(tmp.txtime)) open,
+			max(bankbuy) high, 
+			min(bankbuy) low,
+			(SELECT bankbuy FROM pgmkt WHERE txtime = max(tmp.txtime)) closep
+		FROM (SELECT to_char(txtime, 'YYYY-MM-DD') date, txtime, bankbuy FROM pgmkt ORDER BY txtime) tmp
+		GROUP BY date ORDER BY date`)
+	if err != nil {
+		return nil, fmt.Errorf("query data from table 'pgmkt': %v", err)
+	}
+	defer rows.Close()
+	pgks := make([]pgkday, 0)
+	for rows.Next() {
+		var pgd pgkday
+		if err := rows.Scan(&pgd.Timestamp, &pgd.Open, &pgd.High, &pgd.Low, &pgd.Close); err != nil {
+			return nil, fmt.Errorf("scan rows: %v", err)
+		}
+		if pgd.High != pgd.Low {
+			pgks = append(pgks, pgd)
+		}
+	}
+	return pgks, nil
+}
+
+type tickPrice struct {
+	DB   *sql.DB
+	Mode respFile
+}
+
+func (tp *tickPrice) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	start, err1 := strconv.ParseInt(r.Form.Get("start"), 10, 64)
+	end, err2 := strconv.ParseInt(r.Form.Get("end"), 10, 64)
+	if err1 != nil || err2 != nil {
+		http.Error(w, fmt.Sprintf("request timestamp parameter: start=%v(%v); end=%v(%v)",
+			start, err1, end, err2), http.StatusBadRequest)
+		return
+	}
+	pgcs, err := queryTickData(tp.DB, start, end)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("query papergold tick data: %v", err), http.StatusInternalServerError)
+		return
+	}
+	switch tp.Mode {
+	case typeBinary:
+		w.Header().Set("Content-Type", "application/octet-stream")
+		writeBinaryTick(pgcs, w)
+	case typeJSON:
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		writeJSONTick(pgcs, w)
+	}
+}
+
+type respFile int
+
+const (
+	typeJSON respFile = iota
+	typeBinary
+)
+
+type pgprice struct {
+	Timestamp float64 `json:"t"`
+	Bankbuy   float32 `json:"p"`
+}
+
+func (p pgprice) String() string {
+	tm := time.Unix(int64(p.Timestamp), 0)
+	return fmt.Sprintf("%4d-%02d-%02d %02d:%02d:%02d | %.2f",
+		tm.Year(), tm.Month(), tm.Day(), tm.Hour(), tm.Minute(), tm.Second(), p.Bankbuy)
+}
+
+func queryTickData(db *sql.DB, start, end int64) ([]pgprice, error) {
+	rows, err := db.Query(`
+		SELECT txtmsp, bankbuy
+		FROM (SELECT extract(epoch from txtime) txtmsp, bankbuy FROM pgmkt ORDER BY txtime) tmp
+		WHERE txtmsp >= $1 and txtmsp <= $2`, start, end)
+	if err != nil {
+		return nil, fmt.Errorf("query data from table 'pgmkt': %v", err)
+	}
+	defer rows.Close()
+	pgcs := make([]pgprice, 0)
+	for rows.Next() {
+		var pgc pgprice
+		if err := rows.Scan(&pgc.Timestamp, &pgc.Bankbuy); err != nil {
+			return nil, fmt.Errorf("scan rows: %v", err)
+		}
+		pgcs = append(pgcs, pgc)
+	}
+	return pgcs, nil
+}
+
+func writeJSONTick(pgcs []pgprice, out io.Writer) error {
+	encoder := json.NewEncoder(out)
+	return encoder.Encode(pgcs)
+}
+
+func writeBinaryTick(pgcs []pgprice, out io.Writer) error {
+	num := len(pgcs)
+	if err := binary.Write(out, binary.BigEndian, int32(num)); err != nil {
+		return fmt.Errorf("write record num header: %v", err)
+	}
+	if err := binary.Write(out, binary.BigEndian, pgcs); err != nil {
+		return fmt.Errorf("write records: %v", err)
+	}
+	return nil
+}
