c31da36a74b84909e29a1233748044b1c2a81a93
jaysinco <jaysinco@163.com>
2018-12-05 23:26:37 +0800

    deep model

--- README.md
+++ README.md
@@ -4,19 +4,21 @@ NAME:
    pgold - ICBC paper gold trader assist system
 
 USAGE:
-   pgold.exe [global options] command [command options] [arguments...]
+   main.exe [global options] command [command options] [arguments...]
 
 VERSION:
-   1.0.0
+   0.1.0
 
 COMMANDS:
      market     Crawl market data into database continuously
      export     Export market data from database into file
      import     Import market data from file into database
+     autosave   Autosave market data into file daily
      server     Run http server showing market history data
      realtime   Email trade tips continuously based on policy
      test       Loopback test strategy using history data
      multitask  Run serveral tasks simultaneously
+     dpgen      Generate deep training data
      help, h    Shows a list of commands or help for one command
 
 GLOBAL OPTIONS:
--- policy/deep/train.py
+++ policy/deep/train.py
@@ -1,8 +1,38 @@
 import matplotlib.pyplot as plt  
+import numpy as np
 import struct
 import datetime
+import mxnet as mx
+import logging
 
-f = open('train.dat', 'rb')
+logging.getLogger().setLevel(logging.DEBUG)
+
+def view(sample_list):
+    old_action = ""
+    xbuffer = []
+    ybuffer = []
+    price = 0
+    for index, sample in enumerate(sample_list):
+        price += sample[1]
+        current_action = "buy" if sample[2][0] == 1 else ("sell" if sample[2][1] == 1 else "hold")
+        if current_action != old_action:
+            if old_action != "":
+                xbuffer.append(index)
+                ybuffer.append(price)
+                plt.plot(xbuffer, ybuffer, "red" if old_action == "buy" else ("green" if old_action == "sell" else "gray"), 
+                    linewidth=2 if old_action != "hold" else 1)
+                xbuffer = []
+                ybuffer = []
+            old_action = current_action
+        xbuffer.append(index)
+        ybuffer.append(price)
+    if len(xbuffer) != 0:
+        plt.plot(xbuffer, ybuffer, "red" if old_action == "buy" else ("green" if old_action == "sell" else "gray"), 
+                    linewidth=2 if old_action != "hold" else 1)
+    plt.show()
+
+
+f = open('raw_data.dp', 'rb')
 raw = f.read()
 f.close()
 size, = struct.unpack("<i", raw[:4])
@@ -30,27 +60,47 @@ for i in range(size):
 
 print("{} sample to read".format(size))
 
+range_count = 10
+train_test_ratio = 5/1
+total = len(samples) - range_count + 1
+total_x = np.zeros(shape=(total, range_count))
+total_y = np.zeros(shape=(total))
 
-def view_sample(sample_list):
-    old_action = ""
-    xbuffer = []
-    ybuffer = []
-    price = 0
-    for index, sample in enumerate(sample_list):
-        price += sample[1]
-        current_action = "buy" if sample[2][0] == 1 else ("sell" if sample[2][1] == 1 else "hold")
-        if current_action != old_action:
-            if old_action != "":
-                xbuffer.append(index)
-                ybuffer.append(price)
-                plt.plot(xbuffer, ybuffer, "red" if old_action == "buy" else ("green" if old_action == "sell" else "gray"), 
-                    linewidth=2 if old_action != "hold" else 1)
-                xbuffer = []
-                ybuffer = []
-            old_action = current_action
-        xbuffer.append(index)
-        ybuffer.append(price)
-    if len(xbuffer) != 0:
-        plt.plot(xbuffer, ybuffer, "red" if old_action == "buy" else ("green" if old_action == "sell" else "gray"), 
-                    linewidth=2 if old_action != "hold" else 1)
-    plt.show()
+for i in range(total):
+    total_y[i] = samples[i+range_count-1][2].index(1)
+    for j in range(range_count):
+        total_x[i][j] = samples[i+j][1]
+
+train_n = int(total/(train_test_ratio+1)*train_test_ratio)
+train_x = total_x[:train_n]
+train_y = total_y[:train_n]
+test_x = total_x[train_n:]
+test_y = total_y[train_n:]
+
+batch_size = 100
+ctx = mx.cpu()
+train_iter = mx.io.NDArrayIter(train_x, train_y, batch_size, shuffle=True)
+test_iter = mx.io.NDArrayIter(test_x, test_y, batch_size)
+
+data = mx.sym.var('data')
+fc1  = mx.sym.FullyConnected(data=data, num_hidden=128)
+act1 = mx.sym.Activation(data=fc1, act_type="relu")
+fc2  = mx.sym.FullyConnected(data=act1, num_hidden=64)
+act2 = mx.sym.Activation(data=fc2, act_type="relu")
+fc3  = mx.sym.FullyConnected(data=act2, num_hidden=3)
+mlp  = mx.sym.SoftmaxOutput(data=fc3, name='softmax')
+
+mlp_model = mx.mod.Module(symbol=mlp, context=ctx)
+# mlp_model.bind(data_shapes=train_iter.provide_data, label_shapes=train_iter.provide_label)
+# mlp_model.init_params(initializer=mx.init.Xavier(magnitude=2.))
+
+def train():
+    mlp_model.fit(train_iter,  # train data
+                eval_data=test_iter,  # validation data
+                optimizer='sgd',  # use SGD to train
+                optimizer_params={'learning_rate':0.01},  # use fixed learning rate
+                eval_metric='acc',  # report accuracy during training
+                batch_end_callback = mx.callback.Speedometer(batch_size, 1000), # output progress for each 100 data batches
+                num_epoch=1)
+
+# mlp_model.score(mx.io.NDArrayIter(train_x[:300], train_y[:300], batch_size), mx.metric.Accuracy())
